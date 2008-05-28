e49dac067ac474a90334c0037085f743ba63bc49
Willy Tarreau <w@1wt.eu>
2008-05-28 20:54:12 +0200

    tools: support $PKGROOT to find packages

--- tools/list-to-pkgdir.sh
+++ tools/list-to-pkgdir.sh
@@ -1,1 +1,10 @@
-while read ; do if [ -e /var/flx-pkg/$REPLY ]; then echo /var/flx-pkg/$REPLY; elif [ -e /var/flx-pkg/${REPLY}-flx* ]; then echo /var/flx-pkg/${REPLY}-flx*; else echo $REPLY;fi; done
+#!/bin/bash
+while read; do
+    if [ -e ${PKGROOT:-/var/flx-pkg}/$REPLY ]; then
+        echo ${PKGROOT:-/var/flx-pkg}/$REPLY
+    elif [ -e ${PKGROOT:-/var/flx-pkg}/${REPLY}-flx* ]; then
+        echo ${PKGROOT:-/var/flx-pkg}/${REPLY}-flx*
+    else
+        echo $REPLY
+    fi
+done
